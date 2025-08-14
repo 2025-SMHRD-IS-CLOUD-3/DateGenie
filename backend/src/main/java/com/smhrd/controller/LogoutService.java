474236877b0021cc332922b6f4cae492fa75d52e
Jinyeungseo <rkskek1101@gmail.com>
2025-08-14 09:23:54 +0900

    add backend files

---
+++ backend/src/main/java/com/smhrd/controller/LogoutService.java
@@ -0,0 +1,28 @@
+package com.smhrd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
+
+@WebServlet("/LogoutService")
+public class LogoutService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 로그아웃 : 세션의 데이터 모두 지우기
+		// 1. session 꺼내오기
+		HttpSession session = request.getSession();
+		// 2. session 처리하기 => 세션을 무효화 시킨다.
+		session.invalidate();
+		// 3. redirect방식으로 main.jsp로 이동
+		response.sendRedirect("main.jsp");
+	}
+
+}
